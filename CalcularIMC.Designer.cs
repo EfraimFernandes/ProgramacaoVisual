07ef14de7d854f7cbf0d71705746f485b9f6bab3
Efraim Fernandes <efraim.fernandes@outlook.com>
2024-09-01 00:05:46 -0300

    Calculo de IMC

---
+++ CalcularIMC.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ProgramacaoVisual
+{
+    partial class CalcularIMC
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelAltura = new Label();
+            textBoxAltura = new TextBox();
+            buttonCalcular = new Button();
+            textBoxPeso = new TextBox();
+            labelPeso = new Label();
+            labelResultado = new Label();
+            SuspendLayout();
+            // 
+            // labelAltura
+            // 
+            labelAltura.AutoSize = true;
+            labelAltura.Font = new Font("Verdana", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelAltura.Location = new Point(170, 124);
+            labelAltura.Name = "labelAltura";
+            labelAltura.Size = new Size(138, 42);
+            labelAltura.TabIndex = 0;
+            labelAltura.Text = "Altura:";
+            // 
+            // textBoxAltura
+            // 
+            textBoxAltura.Font = new Font("Verdana", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBoxAltura.Location = new Point(314, 135);
+            textBoxAltura.Name = "textBoxAltura";
+            textBoxAltura.Size = new Size(254, 37);
+            textBoxAltura.TabIndex = 1;
+            // 
+            // buttonCalcular
+            // 
+            buttonCalcular.BackColor = SystemColors.ActiveCaption;
+            buttonCalcular.Cursor = Cursors.Hand;
+            buttonCalcular.Font = new Font("Verdana", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            buttonCalcular.Location = new Point(260, 270);
+            buttonCalcular.Name = "buttonCalcular";
+            buttonCalcular.Size = new Size(212, 41);
+            buttonCalcular.TabIndex = 2;
+            buttonCalcular.Text = "Calcular!";
+            buttonCalcular.UseVisualStyleBackColor = false;
+            buttonCalcular.Click += buttonCalcular_Click_1;
+            // 
+            // textBoxPeso
+            // 
+            textBoxPeso.Font = new Font("Verdana", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBoxPeso.Location = new Point(314, 198);
+            textBoxPeso.Name = "textBoxPeso";
+            textBoxPeso.Size = new Size(254, 37);
+            textBoxPeso.TabIndex = 4;
+            // 
+            // labelPeso
+            // 
+            labelPeso.AutoSize = true;
+            labelPeso.Font = new Font("Verdana", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelPeso.Location = new Point(203, 188);
+            labelPeso.Name = "labelPeso";
+            labelPeso.Size = new Size(105, 38);
+            labelPeso.TabIndex = 3;
+            labelPeso.Text = "Peso:";
+            // 
+            // labelResultado
+            // 
+            labelResultado.BackColor = Color.Linen;
+            labelResultado.Font = new Font("Verdana", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelResultado.Location = new Point(110, 355);
+            labelResultado.Name = "labelResultado";
+            labelResultado.Size = new Size(572, 69);
+            labelResultado.TabIndex = 5;
+            labelResultado.Text = "...";
+            labelResultado.TextAlign = ContentAlignment.MiddleCenter;
+            labelResultado.Click += labelResultado_Click;
+            // 
+            // CalcularIMC
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(labelResultado);
+            Controls.Add(textBoxPeso);
+            Controls.Add(labelPeso);
+            Controls.Add(buttonCalcular);
+            Controls.Add(textBoxAltura);
+            Controls.Add(labelAltura);
+            Name = "CalcularIMC";
+            Text = "CalcularIMC";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label labelAltura;
+        private Button buttonCalcular;
+        private TextBox textBoxPeso;
+        private Label labelPeso;
+        private Label labelResultado;
+        public TextBox textBoxAltura;
+    }
+}
---
+++ CalcularIMC.cs
@@ -0,0 +1,62 @@
+namespace ProgramacaoVisual
+{
+    public partial class CalcularIMC : Form
+    {
+        public CalcularIMC()
+        {
+            InitializeComponent();
+        }
+
+        private void labelResultado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCalcular_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                double altura = Convert.ToDouble(textBoxAltura.Text);
+                double peso = Convert.ToDouble(textBoxPeso.Text);
+
+                double imc = peso / (altura * altura);
+
+                string resultado;
+                if (imc < 18.5)
+                {
+                    resultado = "Abaixo do peso";
+                }
+                else if (imc >= 18.5 && imc < 24.9)
+                {
+                    resultado = "Peso normal";
+                }
+                else if (imc >= 25 && imc < 29.9)
+                {
+                    resultado = "Sobrepeso";
+                }
+                else if (imc >= 30 && imc < 34.9)
+                {
+                    resultado = "Obesidade grau 1";
+                }
+                else if (imc >= 35 && imc < 39.9)
+                {
+                    resultado = "Obesidade grau 2";
+                }
+                else
+                {
+                    resultado = "Obesidade grau 3";
+                }
+
+                labelResultado.Text = $"IMC: {imc:F2} - {resultado}";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, insira valores válidos para altura e peso.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocorreu um erro: {ex.Message}");
+            }
+        }
+    }
+}
